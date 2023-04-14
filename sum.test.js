35ebb36ec304eaf8e25715da3cb11eabe69ebdaf
HernanySimao <hernanysimao123@gmail.com>
2023-04-14 14:05:16 +0100

    aprendendo a fazer multiplos it ou test em uma unica funcao sum ()

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,10 @@
-const sum = require('./sum');
+const sum = require("./sum");
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-});
+describe('soma', () => {
+    it('1+2 equal 3', () => {
+        expect(sum(1, 2)).toBe(3);
+    })
+    it('3+ 2 equal 5', () => {
+        expect(sum(3, 2)).toBe(5);
+    })
+})
